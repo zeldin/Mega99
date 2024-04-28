3b028e60f8acccdfaa166e62c7465b679ed49c45
Marcus Comstedt <marcus@mc.pp.se>
2024-04-28 19:09:46 +0200

    Complete SD card init

--- bootcode/sdcard.c
+++ bootcode/sdcard.c
@@ -71,7 +71,7 @@ static uint8_t sdcard_getresponse(void)
 
 static uint8_t sdcard_docmd_nodeselect(uint8_t cmd, uint32_t param)
 {
-  sdcard_sendbyte(cmd);
+  sdcard_sendbyte(0x40 | cmd);
   sdcard_sendbyte(param >> 24);
   sdcard_sendbyte(param >> 16);
   sdcard_sendbyte(param >> 8);
@@ -92,7 +92,7 @@ static uint8_t sdcard_docmd_noparam(uint8_t cmd)
   return sdcard_docmd(cmd, 0u);
 }
 
-static uint32_t sdcard_getr7(void)
+static uint32_t sdcard_getextresponse(void)
 {
   uint32_t r = sdcard_sendbyte(0);
   r <<= 8; r |= sdcard_sendbyte(0);
@@ -108,11 +108,47 @@ void sdcard_activate()
   display_printf("Activating card\n");
   REGS_SDCARD.ctrl = SPI_SPEED(400000) << 8u;
   sdcard_deselect();
-  uint8_t r1 = sdcard_docmd_noparam(0x40);
+  uint8_t r1;
+  uint32_t time0 = timer_read();
+  do {
+    r1 = sdcard_docmd_noparam(0);
+    if ((timer_read() - time0) > MS_TO_TICKS(2000))
+      break;
+  } while(r1 != 1u);
   display_printf("CMD0, R1 = %x\n", r1);
-  if (r1 != 0x01)
+  if (r1 != 1u)
     return;
-  r1 = sdcard_docmd_nodeselect(0x48, 0x1aa);
-  uint32_t r7 = sdcard_getr7();
+  r1 = sdcard_docmd_nodeselect(8, 0x1aa);
+  uint32_t r7 = sdcard_getextresponse();
   display_printf("CMD8, R7 = %x %x\n", r1, r7);
+  uint32_t p = 0;
+  if (!(r1 & 4u) && r7 == 0x1aa) {
+    display_printf("Card is SD2\n");
+    p = 0x40000000;
+  } else {
+    display_printf("Card is SD1\n");
+  }
+  time0 = timer_read();
+  do {
+    r1 = sdcard_docmd_noparam(55);
+    if ((r1 & ~1)) {
+      display_printf("CMD55, R1 = %x\n", r1);
+      return;
+    }
+    r1 = sdcard_docmd(41, p);
+    if ((timer_read() - time0) > MS_TO_TICKS(2000))
+      break;
+  } while (r1 == 1);
+  display_printf("ACMD41, R1 = %x\n", r1);
+  if (r1 != 0)
+    return;
+  if (p) {
+    r1 = sdcard_docmd_nodeselect(58, 0);
+    uint32_t r3 = sdcard_getextresponse();
+    display_printf("CMD58, R3 = %x %x\n", r1, r3);
+    if (r1 != 0)
+      return;
+    if (((r3 >> 30) & 3u) == 3u)
+      display_printf("Card is SDHC\n");
+  }
 }
