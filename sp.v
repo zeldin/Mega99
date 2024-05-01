df0e9c58f7eac1dcf613fc51ef74b8076523fb75
Marcus Comstedt <marcus@mc.pp.se>
2024-05-01 10:08:09 +0200

    Configure mor1kx enabled instruction set

--- sp.v
+++ sp.v
@@ -96,7 +96,8 @@ module sp(input         clk,
    end
 
    mor1kx #(.OPTION_OPERAND_WIDTH(32), .BUS_IF_TYPE("WISHBONE32"),
-	    .FEATURE_TRACEPORT_EXEC("ENABLED"))
+	    .FEATURE_CMOV("ENABLED"), .FEATURE_EXT("ENABLED"),
+	    .FEATURE_ATOMIC("NONE"), .FEATURE_TRACEPORT_EXEC("ENABLED"))
    or1k (.clk(clk), .rst(reset),
 
 	 .iwbm_adr_o(or1k_i_adr), .iwbm_stb_o(or1k_i_stb),
